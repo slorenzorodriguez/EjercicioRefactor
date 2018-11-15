31fd039b75fb2bc18395580577810f5676c8833d
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2018-11-15 09:33:41 +0100

    Refactor/ Nombre variables cambiado.

--- src/ejerciciorefactor/Refactor3.java
+++ src/ejerciciorefactor/Refactor3.java
@@ -19,17 +19,17 @@ public class Refactor3 {
     //LIstar los numeros según el numero de digitos indicado
     //Considero solo hasta numeros menores a 100000 (5 digitos), 
     //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
-    public static boolean p = false;
+    public static boolean segundoOperando = false;
 
     public static void main(String arg[]) {
-        int numDigitos = 0;
-        int ndigitos = 0;
-        numDigitos = Integer.parseInt(arg[0]);
-        if (numDigitos <= 0) {
+        int numeroDigitos = 0;  //nombre variable cambiado de 'numDigitos' a 'numeroDigitos'
+        int numerodigitos = 0;  //nombre variable cambiado de 'ndigitos' a 'numerodigitos'
+        numeroDigitos = Integer.parseInt(arg[0]);
+        if (numeroDigitos <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        for (int i = 1; i <= 99999; i++) {
-            int divisionEntera = i;
+        for (int primerOperando = 1; primerOperando <= 99999; primerOperando++) { ////nombre variable cambiado de 'i' a 'primerOperando'
+            int divisionEntera = primerOperando;
 
             int contador = 0;
 
@@ -37,40 +37,40 @@ public class Refactor3 {
                 divisionEntera = divisionEntera / 10;
                 contador++;
             }
-            ndigitos = contador;
+            numerodigitos = contador;
 
-            if (ndigitos == numDigitos) {
-                if (i < 4) {
-                    p = true;
+            if (numerodigitos == numeroDigitos) {
+                if (contador < 4) {
+                    segundoOperando = true; ////nombre variable cambiado de 'p' a 'segundoOperando'
                 } else {
-                    if (i % 2 == 0) {
-                        p = false;
+                    if (contador % 2 == 0) {
+                        segundoOperando = false;
                     } else {
                         int contador1 = 0;
-                        int i1 = 1;
-                        int limite = (i - 1) / 2;
+                        int tercerOperando = 1; ////nombre variable cambiado de 'il' a 'tercerOperando'
+                        int limite = (contador - 1) / 2;
                         if (limite % 2 == 0) {
                             limite--;
                         }
 
-                        while (i1 <= limite) {
-                            if (i % i1 == 0) {
+                        while (tercerOperando <= limite) {
+                            if (contador % tercerOperando == 0) {
                                 contador1++;
                             }
-                            i1 += 2;
+                            tercerOperando += 2;
                             if (contador1 == 2) {
-                                i1 = limite + 1;
+                                tercerOperando = limite + 1;
                             }
                         }
 
                         if (contador1 == 1) {
-                            p = true;
+                            Refactor3.segundoOperando = true;
                         }
                     }
                 }
 
-                if (p == true) {
-                    System.out.println(i);
+                if (segundoOperando == true) {
+                    System.out.println(contador);
                 }
             }
         }
